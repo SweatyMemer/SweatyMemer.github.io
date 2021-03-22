7a6d6c140b0083c7449856585edaa2f324a6e9d5
darcl <ds123shep@gmail.com>
2021-03-22 19:20:46 +1000

    Updated

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function epilepsy()
+{
+    Boolean i = true;
+    while (i == true) {
+        document.getElementById('epil').style.backgroundColor = 'white';
+        document.getElementById('epil').style.backgroundColor = 'black';
+        document.getElementById('epil').style.backgroundColor = 'red';
+        document.getElementById('epil').style.backgroundColor = 'yellow';
+        document.getElementById('epil').style.backgroundColor = 'lime';
+        document.getElementById('epil').style.backgroundColor = 'aqua';
+        document.getElementById('epil').style.backgroundColor = 'green';
+        document.getElementById('epil').style.backgroundColor = 'blue';
+        document.getElementById('epil').style.backgroundColor = 'pink';
+    }
+}
